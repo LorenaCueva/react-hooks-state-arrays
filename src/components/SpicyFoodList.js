b3ba8dcd6a680a1e688f8a78e7f222d06f9fd9b7
Lorena Cueva <lorenacueva@gmail.com>
2022-06-10 16:49:53 -0700

    commit tree 9b8f3a77b9dc0435a33bcd78974c22975378f34c

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,14 +3,33 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodList = [...foods, newFood];
+    setFoods(newFoodList);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  // function handleDeleteFood(id){
+  //   const newFoodList = foods.filter(food => food.id !== id);
+  //   setFoods(newFoodList);
+  // }
+
+  function handleClick(id){
+    const newFoodList = foods.map(food => food.id === id ? {...food, heatLevel : food.heatLevel + 1} : food);
+    setFoods(newFoodList);
+  }
+
+  function handleFilterChange(e){
+    setFilterBy(e.target.value);
+  }
+  
+  const foodsToDisplay = foods.filter(food => filterBy === "All" ? true : food.cuisine === filterBy);
+
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -19,6 +38,13 @@ function SpicyFoodList() {
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
     </div>
   );
 }
